1198c73ac927bee36cc51a9581d6aebcefb6c7b0
Luke Sheard <me@lukesheard.com>
2016-09-25 14:47:39 +0100

    Change font name

--- bin/install-font.sh
+++ bin/install-font.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir /tmp/adodefont
 cd /tmp/adodefont
-wget https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.zip
+wget https://github.com/adobe-fonts/source-sans-pro/archive/2.020R-ro/1.075R-it.zip
 unzip 1.030R-it.zip
 mkdir -p ~/.fonts
 cp source-code-pro-2.010R-ro-1.030R-it/OTF/*.otf ~/.fonts/
